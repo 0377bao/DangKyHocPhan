7472549201bffa3ca79e49410e51a24c7b994529
manhhieu120499 <vomanhhieu62@gmail.com>
2024-05-02 12:02:31 +0700

    init project

---
+++ backend/DangKyHocPhan/src/main/java/iuh/dangkyhocphan/DangKyHocPhanApplication.java
@@ -0,0 +1,13 @@
+package iuh.dangkyhocphan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangKyHocPhanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DangKyHocPhanApplication.class, args);
+    }
+
+}
---
+++ backend/DangKyHocPhan/src/test/java/iuh/dangkyhocphan/DangKyHocPhanApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.dangkyhocphan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangKyHocPhanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
